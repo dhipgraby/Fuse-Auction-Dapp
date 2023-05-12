c39bf3666d98a7f363a9a2ca60306147f2586039
kennow <kenneth.zambrano4@gmail.com>
2023-05-12 10:43:28 +0200

    Build fixes and readme

---
+++ README.md
@@ -0,0 +1,3 @@
+## Fuse Auction Dapp 🎮
+
+Create Auctions for NFTs! Support ERC20 token auctions.
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,13 @@
 import styles from "../styles/Home.module.css";
-import NFTContract from "../components/interfaces/NFTContract";
 
 export default function Home() {
   return (
     <div>
       <main className={styles.main}>
-      <h1>Welcome to Fuse Auction</h1>
-      <p>
-        NFT Auction! Bid and buy nfts at best price
-      </p>
+        <h1>Welcome to Fuse Auction</h1>
+        <p>
+          NFT Auction! Bid and buy nfts at best price
+        </p>
       </main>
     </div>
   );
